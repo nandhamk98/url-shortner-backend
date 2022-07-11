8f59925c3083711f8c40374864f343b6b2915983
Nandhakumar <nandhakumarmk98@gmail.com>
2022-07-11 18:00:46 +0530

    moved file

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
 import { userRouter } from "./UserRoute.js";
-import { urlShortnerRouter } from "./Routes/UrlShortnerRoute.js";
+import { urlShortnerRouter } from "./UrlShortnerRoute.js";
 
 dotenv.config();
 
